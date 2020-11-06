269f755920af1c536cce9e11f33d4a85c9bfbf21
Alexander <brixzen@hotmail.com>
2020-11-06 05:24:54 +0100

    small reformatting

--- mydns.py
+++ mydns.py
@@ -54,26 +54,10 @@ def send_udp_message(message, address, port):
     return binascii.hexlify(data).decode("utf-8")
 
 
-
 url = reformatHex()
-
-
 message = "AA AA 01 00 00 01 00 00 00 00 00 00 " \
 + url+ "00 01 00 01"
 
-
-#message = "AA AA 01 00 00 01 00 00 00 00 00 00 " \
-#"07 65 78 61 6d 70 6c 65 03 63 6f 6d 00 00 01 00 01"
-########################################
-######domain that we are requesting#####
-
-
-#MaryLand root DNS server 
 response = send_udp_message(message, rootDNS, 53)
 print("\n***Server Reply Content***\n",response)
 print("\n***IP for queried domain name***\n")
-#Writes the incoming message to a text file
-#f.write(format_hex(response))
-#f.close()
-
-#print (reformatHex())
